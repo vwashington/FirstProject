d0ad8c3696904f7e09bd000fd5042c1e9591944a
Antonio Gage <antoniojgage@gmail.com>
2014-09-22 16:55:50 -0500

    Removing Comments

--- src/VolcanoRobot.java
+++ src/VolcanoRobot.java
@@ -4,9 +4,7 @@ public class VolcanoRobot {
 	public static void main(String[] args) {
 //Creating VolcanoRobot Class
 //Testing JR Can you see this
-		//Jr blackguy is awesome we LURNIN CODE TOGETHER FINALLY
-		//gustavo already knows this stuff
-		//testing from work comp, AT work....with proxy
+		
 	}
 
 }
