d94cc31c2d4bc4a1217b05d962ad4bb79f22b2ca
antoniojgage <antoniojgage@antonio-gages-macbook-pro.local>
2014-10-07 22:57:25 -0500

    Test From mac

--- src/VolcanoRobot.java
+++ src/VolcanoRobot.java
@@ -1,10 +1,10 @@
-
 public class VolcanoRobot {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 //Creating VolcanoRobot Class
 //Testing JR Can you see this
 		//Jr blackguy is awesome we LURNIN CODE TOGETHER FINALLY
+		//im coding bruh MAC DIZZLE
 	}
 
 }
